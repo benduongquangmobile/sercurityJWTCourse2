8179f5fd2ad27347b4c5f2d7f833eeff6ceef0e8
binhduongquang <binhduongquangvtca@gmail.com>
2022-02-28 18:53:29 +0700

    configure Spring Security Authorization

---
+++ src/main/java/com/ben/sercurity/SercurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.ben.sercurity;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SercurityConfiguration extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    auth
+        .inMemoryAuthentication()
+        .withUser("user")
+        .password(encoder.encode("user"))
+        .roles("USER")
+        .and()
+        .withUser("admin")
+        .password(encoder.encode("admin"))
+        .roles("ADMIN");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http.authorizeRequests()
+        .antMatchers("/user").hasRole("USER")
+        .antMatchers("/admin").hasAnyRole("ADMIN", "USER")
+        .antMatchers("/").permitAll()
+        .and().formLogin();
+  }
+
+}
